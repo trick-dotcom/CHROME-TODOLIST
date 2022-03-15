5bb99ee90fbc599adc988909a8aa63396b76bc6a
trick-dotcom <95709645+trick-dotcom@users.noreply.github.com>
2022-03-16 02:23:27 +0900

    restart

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images=["01.jpeg", "02.jpg", "03.jpg"];
-
-const chosenImage=images[Math.floor(Math.random()*images.length)];
-
-const bgImages = document.createElement("img");
-
-bgImages.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImages);
