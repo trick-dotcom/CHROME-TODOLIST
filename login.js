e37f8808c0025d11d2e8e3c560372c6bb3aae24e
trick-dotcom <95709645+trick-dotcom@users.noreply.github.com>
2022-03-18 02:20:26 +0900

    new

--- login.js
+++ login.js
@@ -1,9 +1,1 @@
-const loginUser = document.querySelector(".login_user input");
-const nameOfUser = document.querySelector(".welcome");
-
-function handleLoginForm(){
-    console.log(loginUser.value)
-}
-
-
-loginUser.addEventListener("submit", handleLoginForm);
+alert("hi");
